2605d686d1cdf0e797fe2e15de9b9c2edf7df615
maheshbhudia <mahesh.bhudia@kcl.ac.uk>
2016-02-07 15:13:45 +0000

    Now added password validation and confirmation with a specific

--- app/src/main/java/com/example/lee/projectrun/RegisterActivity.java
+++ app/src/main/java/com/example/lee/projectrun/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,11 +21,15 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText txtFirstName;
     private EditText txtLastName;
     private EditText txtEmailAddress;
-    private String stringFirstName, stringLastName, stringEmail;
+    private EditText txtPassword;
+    private EditText txtConfirmPassword;
+    private String stringFirstName, stringLastName, stringEmail, stringPassword, stringConfirmPassword;
     private Button btnRegister;
     private Boolean booleanFirstName = true;
     private Boolean booleanLastName = true;
     private Boolean booleanEmail = true;
+    private Boolean booleanPassword = true;
+    private Boolean booleanConfirmPassword = true;
     private static String letterList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static Random random = new Random();
 
@@ -36,6 +41,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         txtFirstName = (EditText) findViewById(R.id.txtFirstName);
         txtLastName = (EditText) findViewById(R.id.txtLastName);
         txtEmailAddress = (EditText) findViewById(R.id.txtEmailAddress);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        txtConfirmPassword = (EditText) findViewById(R.id.txtConfirmPassword);
         btnRegister = (Button) findViewById(R.id.btnRegister);
 
         btnRegister.setOnClickListener(this);
@@ -75,6 +82,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         stringFirstName = txtFirstName.getText().toString();
         stringLastName = txtLastName.getText().toString();
         stringEmail = txtEmailAddress.getText().toString();
+        stringPassword = txtPassword.getText().toString();
+        stringConfirmPassword = txtConfirmPassword.getText().toString();
         if(TextUtils.isEmpty(stringFirstName)) {
             booleanFirstName = false;
             txtFirstName.setError("Please Enter Your First Name");
@@ -85,20 +94,36 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
         if(TextUtils.isEmpty(stringLastName)) {
             booleanLastName = false;
-            txtLastName.setError("Please Enter Your Last Name");
+            txtLastName.setError("Please enter Your Last Name");
             return;
         }
         else{
             booleanLastName = true;
         }
-        if (!isValidEmail(stringEmail)) {
+        if(!isValidEmail(stringEmail)) {
             booleanEmail = false;
-            txtEmailAddress.setError("Please Enter a Valid KCL Email");
+            txtEmailAddress.setError("Please enter a valid KCL Email");
         }
         else{
             booleanEmail = true;
         }
-        if(booleanFirstName == true && booleanLastName == true && booleanEmail == true) {
+        if(!isValidPassword(stringPassword)){
+            booleanPassword = false;
+            txtPassword.setError("Please enter a valid Password, MIN 6 characters and at least 1 number");
+        }
+        else{
+            booleanPassword = true;
+        }
+
+        if(!stringConfirmPassword.equals(stringPassword)){
+            booleanConfirmPassword = false;
+            txtConfirmPassword.setError("The passwords do not match");
+        }
+        else{
+            booleanConfirmPassword = true;
+        }
+
+        if(booleanFirstName == true && booleanLastName == true && booleanEmail == true && booleanPassword == true && booleanConfirmPassword == true) {
             emailSend();
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
@@ -111,11 +136,18 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
      * @return
      */
     private boolean isValidEmail(String emailString) {
-        //String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
         String emailPattern = "^[A-Za-z0-9]+(\\.[A-Za-z]+)*@kcl+\\.ac\\.uk$";
 
         Pattern pattern = Pattern.compile(emailPattern);
         Matcher matcher = pattern.matcher(emailString);
+        return matcher.matches();
+    }
+
+    private boolean isValidPassword(String passwordString) {
+        String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$";
+
+        Pattern pattern = Pattern.compile(passwordPattern);
+        Matcher matcher = pattern.matcher(passwordString);
         return matcher.matches();
     }
 
