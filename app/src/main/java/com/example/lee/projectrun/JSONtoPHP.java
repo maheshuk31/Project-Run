f9a1e3c49fcb250a0da0c36a70ea987fda84b10a
Darren Middleton <k1462486@kcl.ac.uk>
2016-02-20 18:34:05 +0000

    PHP to JSON start

---
+++ app/src/main/java/com/example/lee/projectrun/JSONtoPHP.java
@@ -0,0 +1,21 @@
+package com.example.lee.projectrun;
+
+
+public class JSONtoPHP {
+
+
+
+    /**
+     * Use for email registration
+     */
+    public void createUser(String Name, String Password, String Email){
+
+    }
+
+    /**
+     *
+     */
+    public void modifyUser(){
+
+    }
+}
